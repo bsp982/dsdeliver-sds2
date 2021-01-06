01ec9e467b799038a4474f409dfb2b8cd9bb2fbc
Bruno <brunosantos.engproducao@gmail.com>
2021-01-06 19:30:04 -0300

    fix

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -30,9 +30,9 @@ public class OrderService {
         return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
     }
 
-    @Transactional()
+    @Transactional
     public OrderDTO insert(OrderDTO dto) {
-        Order order = new Order(null,dto.getAddress(),dto.getLatitude(),dto.getLongitude(),Instant.now(),OrderStatus.PENDING);
+        Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
 
         for (ProductDTO p : dto.getProducts()) {
             Product product = productRepository.getOne(p.getId());
@@ -40,6 +40,14 @@ public class OrderService {
         }
         order = repository.save(order);
 
+        return new OrderDTO(order);
+    }
+
+    @Transactional
+    public OrderDTO setDelivered(Long id) {
+        Order order = repository.getOne(id);
+        order.setStatus(OrderStatus.DELIVERED);
+        order = repository.save(order);
         return new OrderDTO(order);
     }
 
